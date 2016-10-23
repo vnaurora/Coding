02168adf8933cd2c22538a9821f6d713c4a5d577
vnaurora <nguyenhuuquang@gmail.com>
2016-10-23 15:49:28 -0700

    Add Easy/ReverseInteger.cpp and Medium/SearchRange.cpp.

--- Algorithms/Medium/LongestStringWithoutDuplicates.cpp
+++ Algorithms/Medium/LongestStringWithoutDuplicates.cpp
@@ -5,6 +5,7 @@
 //Given "abcabcbb", the answer is "abc", which the length is 3.
 //Given "bbbbb", the answer is "b", with the length of 1.
 //Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+//Ref: https://tenderleo.gitbooks.io/leetcode-solutions-/content/GoogleMedium/3.html
 
 #include <string>
 #include <iostream>
